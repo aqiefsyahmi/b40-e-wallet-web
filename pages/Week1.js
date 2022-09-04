ed5e30eda44979a58b7b677e747e85f19d127377
Aqief Syahmi <aqiefsyahmi@gmail.com>
2022-09-04 17:46:55 +0800

    initial commit

--- pages/Week1.js
+++ pages/Week1.js
@@ -5,10 +5,10 @@ import { getTransactions } from "../lib/getTransactions";
 import { useTime } from "../hooks";
 
 const dummyData = [
-  { id: 1, date: "1 Aug - 7 Aug" },
-  { id: 2, date: "8 Aug - 14 Aug" },
-  { id: 3, date: "15 Aug - 21 Aug" },
-  { id: 4, date: "22 Aug - 28 Aug" },
+  { id: 1, cafename: "Kafe Mamada", totalrm: 100 },
+  { id: 2, cafename: "Pak Amid Kafe", totalrm: 140 },
+  { id: 3, cafename: "GigaChad Cake", totalrm: 250 },
+  { id: 4, cafename: "Kafe Sheeeeeshhh", totalrm: 300 },
 ];
 
 const transactions = () => {
@@ -25,52 +25,45 @@ const transactions = () => {
     // fetchData();
   }, []);
 
-  /* 
-https://www.youtube.com/watch?v=dQw4w9WgXcQ
-TODO List
-1. Buat UI based on design figma/ligma
-2. API call
-
-aku rasa en klau pakai grid css lagi sedap drpd table do
-lebih customize
-Truee
-takpela kita buat asal jadi luh, next update kita guna grid
-LESSSGOOOOO
-
-AYUHHH DAH NAK JADI MAT
-
-JADI SUDAH BOSSKUR
-
-*/
-
   return (
     <Layout>
       <div className="w-2/3 items-center">
-        <h1 className="mb-[30px] font-bold text-3xl">Transactions List</h1>
+        <h1 className="mb-[30px] font-bold text-3xl">
+          Transactions Week 1-7 Aug
+        </h1>
         <div className="p-8 border-[1px] rounded-md bg-[#FFFFFF] border-gray-300">
           <table className="centertable">
             <thead>
               <tr>
                 <td className="w-[6rem]"></td>
-                <td className="pb-[37px] font-medium">Date</td>
-                <td className="w-[8rem]"></td>
+                <td className="pb-[37px] font-medium">Cafe Name</td>
+                <td className="pb-[37px] font-medium w-[8rem] text-center">
+                  Total(RM)
+                </td>
               </tr>
             </thead>
             <tbody>
               {transactions &&
                 transactions.map((data, i) => {
-                  const { id, date } = data;
+                  const { id, cafename, totalrm } = data;
 
                   return (
                     <tr key={i} className="text-gray-500">
                       <td className="pb-6 pr-4 text-center">{id}.</td>
-                      <td className="pb-6">{date}</td>
-                      <td className="pb-6 font-medium">
-                        <button>Show more</button>
-                      </td>
+                      <td className="pb-6">{cafename}</td>
+                      <td className="pb-6 text-center">{totalrm}</td>
                     </tr>
                   );
                 })}
+              <tr className="text-gray-500">
+                <td></td>
+                <td></td>
+                <td className="pb-6 text-center">
+                  <button className="py-1 px-7 text-black font-medium bg-[#FFD400] rounded-md">
+                    Print
+                  </button>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
