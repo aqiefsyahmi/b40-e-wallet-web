3e251fbb1b9fab11796d970d844c5350fcddfc92
Aqief Syahmi <aqiefsyahmi@gmail.com>
2022-09-12 20:18:47 +0800

    initial commit

--- components/NavBar.js
+++ components/NavBar.js
@@ -12,6 +12,7 @@ const NavBar = () => {
   const { remove } = useLocalStorage();
   const [showDrop, setShowDrop] = useState(false);
   const [showDrop1, setShowDrop1] = useState(false);
+  const [showDrop2, setShowDrop2] = useState(false);
 
   const onLogout = () => {
     remove("accessToken");
@@ -50,6 +51,27 @@ const NavBar = () => {
             </div>
           </div>
         </li>
+        <li
+          className="relative cursor-pointer"
+          onMouseEnter={() => setShowDrop1(true)}
+          onMouseLeave={() => setShowDrop1(false)}
+        >
+          <div className="duration-150 hover:font-bold">Users Data</div>
+          <div className={`absolute ${!showDrop1 && `hidden`}`}>
+            <div className="py-3 grid w-[8rem] gap-2 rounded-md bg-white shadow ">
+              <div className="w-[8rem] duration-150 hover:bg-[#d1cfcf]">
+                <div className="grid justify-center duration-150 hover:font-semibold">
+                  <Link href="/addCafe">Cafe Owner</Link>
+                </div>
+              </div>
+              <div className="w-[8rem] duration-150 hover:bg-[#d1cfcf]">
+                <div className="ml-6 duration-150 hover:font-semibold">
+                  <Link href="/addStudent">Student</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
         <li>
           <div className="duration-150 hover:font-bold">
             <Link href="/addWallet">E-Wallet Point</Link>
@@ -57,11 +79,11 @@ const NavBar = () => {
         </li>
         <li
           className="relative cursor-pointer"
-          onMouseEnter={() => setShowDrop1(true)}
-          onMouseLeave={() => setShowDrop1(false)}
+          onMouseEnter={() => setShowDrop2(true)}
+          onMouseLeave={() => setShowDrop2(false)}
         >
           <div className="duration-150 hover:font-bold">Transactions</div>
-          <div className={`absolute ${!showDrop1 && `hidden`}`}>
+          <div className={`absolute ${!showDrop2 && `hidden`}`}>
             <div className="py-3 grid justify-center w-[8rem] gap-2 rounded-md bg-white shadow">
               <div className="w-[8rem] duration-150 hover:bg-[#d1cfcf]">
                 <div className="grid justify-center duration-150 hover:font-semibold">
---
+++ pages/usersdataStudent.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+
+import { Layout, Input } from "../../components";
+import { getTransactions } from "../../lib/getTransactions";
+import { useTime } from "../../hooks";
+import handleTransactions from "../../utils/handleTransactions";
+
+const transactions = () => {
+  const [transactions, setTransactions] = useState([]);
+  const format = useTime();
+  // Filter Student
+  const [searchText, setSearchText] = useState("");
+  //Filter Student
+  const filteredstudent = transactions.filter(
+    ({ student_name, matricNo }) =>
+      student_name.toLowerCase().includes(searchText.toLowerCase()) ||
+      matricNo.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getTransactions();
+
+      setTransactions(handleTransactions({ array: res, student: true }));
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Layout>
+      <div className="w-2/3 items-center">
+        <h1 className="mb-[30px] font-bold text-3xl">
+          Transactions List (Students)
+        </h1>
+        <input
+          className="border w-full px-2 py-2 border-gray-300 rounded-md"
+          type="text"
+          value={searchText}
+          placeholder="Search by Name/ Matric Number..."
+          onChange={({ target }) => setSearchText(target.value)}
+        />
+
+        <div className="mt-4 p-8 border-[1px] rounded-md bg-[#FFFFFF] border-gray-300">
+          <table className="centertable">
+            <thead>
+              <tr>
+                <td className="w-[6rem]"></td>
+                <td className="pb-[37px] font-medium">Student Name</td>
+                <td className="pb-[37px] font-medium">Matric No.</td>
+                <td className="pb-[37px] w-[14rem] font-medium text-center">
+                  Total(RM)
+                </td>
+              </tr>
+            </thead>
+            {filteredstudent &&
+              filteredstudent.map((data, i) => {
+                const { student_name, matricNo, total } = data;
+
+                return (
+                  <tbody>
+                    <tr key={i} className="text-gray-500">
+                      <td className="pb-6 pr-4 text-center">{i + 1}.</td>
+                      <td className="pb-6">{student_name}</td>
+                      <td className="pb-6">{matricNo}</td>
+                      <td className="pb-6 font-medium text-center">{total}</td>
+                    </tr>
+                  </tbody>
+                );
+              })}
+          </table>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default transactions;
