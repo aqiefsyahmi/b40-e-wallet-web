2b02880a1de4c6628758532fcf27b34c9e908bf2
Haziq Musa <last.game01987@gmail.com>
2022-09-25 06:49:16 +0800

    refresh token function

--- pages/api/instance.js
+++ pages/api/instance.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useLocalStorage } from "../../hooks";
 
-const { getItem } = useLocalStorage();
+const { getItem, store } = useLocalStorage();
 
 const instance = axios.create({
-  baseURL: "https://e-voucher-api.herokuapp.com/" /*||"http://localhost:3000"*/,
+  baseURL:
+    // "https://e-voucher-api.herokuapp.com/" || 
+    "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
@@ -18,6 +20,27 @@ instance.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
+  }
+);
+
+// refresh token implementation
+const getRefreshToken = () => {
+  instance.post('/token', { refreshToken: getItem("refreshToken") })
+    .then(token => store('accessToken', token.data.accessToken))
+    .catch(err => console.error('error refresh', err));
+}
+
+instance.interceptors.response.use(
+  response => {
+    return response;
+  },
+  async (error) => {
+    if (error.response.status == 403) {
+      getRefreshToken();
+
+      // return prev request
+      return instance(error.config)
+    }
   }
 );
 
