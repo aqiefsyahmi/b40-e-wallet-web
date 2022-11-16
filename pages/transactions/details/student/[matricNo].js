937d4a16a3b6722186d62b43a0eae33732c68633
Haziq Musa <last.game01987@gmail.com>
2022-11-17 05:36:46 +0800

    refactor & fetch data

--- pages/transactions/details/student/[matricNo].js
+++ pages/transactions/details/student/[matricNo].js
@@ -2,33 +2,35 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import { Layout } from "../../../../components";
-
-import { getTransactionCafeByUsername } from "../../../../lib/getTransactions";
+import { getTransactionStudentByMatric } from "../../../../lib/getTransactions";
 import { formatDate, formatTime } from "../../../../utils/formatTime";
 
-const CafeTransaction = () => {
+const StudentTransaction = () => {
   const router = useRouter();
-  const { username } = router.query;
-  const [transactions, setTransactions] = useState([]);
+  const { matricNo } = router.query;
 
+  const [transactions, setTransactions] = useState([]);
+  console.log(matricNo);
   useEffect(() => {
-    if (username)
-      getTransactionCafeByUsername(username)
+    if (matricNo)
+      getTransactionStudentByMatric(matricNo)
         .then(setTransactions)
         .catch(err => console.log(err));
-  }, [username]);
+  }, [matricNo]);
 
   return (
     <Layout>
       <div className="w-2/3 items-center my-6">
-        <h1 className="font-bold text-3xl">Cafe Transactions Details</h1>
-        <p className="mb-[30px] mt-3">{transactions[0]?.cafe_name}</p>
+        <h1 className="font-bold text-3xl">Student Transaction Details</h1>
+        <p className="mb-[30px] mt-3">
+          {transactions[0]?.student_name} - {matricNo}
+        </p>
         <div className="p-8 border-[1px] rounded-md bg-[#FFFFFF] border-gray-300">
           <table className="centertable">
             <thead>
               <tr>
                 <td className="w-[6rem]"></td>
-                <td className="pb-[37px] font-medium">Sender</td>
+                <td className="pb-[37px] font-medium">Recepient</td>
                 <td className="pb-[37px] font-medium">Date</td>
                 <td className="pb-[37px] font-medium text-center">
                   Amount(RM)
@@ -36,13 +38,13 @@ const CafeTransaction = () => {
               </tr>
             </thead>
             <tbody>
-              {transactions.map((data, i) => {
+              {transactions?.map((data, i) => {
                 return (
                   <tr className="text-gray-500" key={i}>
                     <td className="pb-6 pr-4 text-center">{i + 1}.</td>
-                    <td className="pb-6">{data.student_name}</td>
+                    <td className="pb-6">{data.cafe_name}</td>
                     <td className="pb-6">
-                      {formatDate(data.created_at)} -
+                      {formatDate(data.created_at)} -{" "}
                       {formatTime(data.created_at)}
                     </td>
                     <td className="pb-6 text-center">{data.amount}</td>
@@ -57,4 +59,4 @@ const CafeTransaction = () => {
   );
 };
 
-export default CafeTransaction;
+export default StudentTransaction;
