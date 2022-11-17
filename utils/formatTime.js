ce065ffc3f77c47353231d82bfffe5d4feb87eed
Haziq Musa <last.game01987@gmail.com>
2022-11-17 21:16:04 +0800

    new utils

--- utils/formatTime.js
+++ utils/formatTime.js
@@ -1,9 +1,10 @@
 import moment from "moment";
 
 export const formatDate = date => {
+  // console.log(moment(date));
   return moment(date).format("DD/MM/YYYY");
 };
 
 export const formatTime = time => {
-  return moment(time).format("hh:mma");
+  return moment(time, "hh:mma").format("hh:mma");
 };
