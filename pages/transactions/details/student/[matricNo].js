9344b51a3b47380ca6babed12feb52488a569184
Haziq Musa <last.game01987@gmail.com>
2022-11-18 19:54:07 +0800

    refactor

--- pages/transactions/details/student/[matricNo].js
+++ pages/transactions/details/student/[matricNo].js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 
 import { Layout } from "../../../../components";
 import { getTransactionStudentByMatric } from "../../../../lib/getTransactions";
+import { getStudentByMatric } from "../../../../lib/getStudents";
 import { formatDate, formatTime } from "../../../../utils/formatTime";
 
 const StudentTransaction = () => {
@@ -10,12 +11,19 @@ const StudentTransaction = () => {
   const { matricNo } = router.query;
 
   const [transactions, setTransactions] = useState([]);
-  console.log(matricNo);
+  const [student, setStudent] = useState();
+  const [error, setError] = useState(false);
+
   useEffect(() => {
-    if (matricNo)
+    if (matricNo) {
       getTransactionStudentByMatric(matricNo)
         .then(setTransactions)
+        .catch(() => setError(true));
+
+      getStudentByMatric(matricNo)
+        .then(setStudent)
         .catch(err => console.log(err));
+    }
   }, [matricNo]);
 
   return (
@@ -23,7 +31,7 @@ const StudentTransaction = () => {
       <div className="w-2/3 items-center my-6">
         <h1 className="font-bold text-3xl">Student Transaction Details</h1>
         <p className="mb-[30px] mt-3">
-          {transactions[0]?.student_name} - {matricNo}
+          {`${student?.student_name} (${student?.matric_no})`}
         </p>
         <div className="p-8 border-[1px] rounded-md bg-[#FFFFFF] border-gray-300">
           <table className="centertable">
@@ -38,20 +46,29 @@ const StudentTransaction = () => {
               </tr>
             </thead>
             <tbody>
-              {transactions?.map((data, i) => {
-                return (
-                  <tr className="text-gray-500" key={i}>
-                    <td className="pb-6 pr-4 text-center">{i + 1}.</td>
-                    <td className="pb-6">{data.cafe_name}</td>
-                    <td className="pb-6">
-                      {formatDate(data.created_at)} -{" "}
-                      {formatTime(data.created_at)}
-                    </td>
-                    <td className="pb-6 text-center">{data.amount}</td>
-                  </tr>
-                );
-              })}
+              {!error &&
+                transactions?.map((data, i) => {
+                  return (
+                    <tr className="text-gray-500" key={i}>
+                      <td className="pb-6 pr-4 text-center">{i + 1}.</td>
+                      <td className="pb-6">{data.cafe_name}</td>
+                      <td className="pb-6">
+                        {formatDate(data.created_on)} -{" "}
+                        {formatTime(data.created_at)}
+                      </td>
+                      <td className="pb-6 text-center">{data.amount}</td>
+                    </tr>
+                  );
+                })}
             </tbody>
+            {error && (
+              <tr className="text-gray-500">
+                <td className="pb-6 pr-4 text-center">1.</td>
+                <td className="pb-6">Not found</td>
+                <td className="pb-6">Not found</td>
+                <td className="pb-6 text-center">Not found</td>
+              </tr>
+            )}
           </table>
         </div>
       </div>
