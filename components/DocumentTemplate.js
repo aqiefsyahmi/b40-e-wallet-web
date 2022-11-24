c42f8f826f2e5d034e96568555a9a51f02021a91
Haziq Musa <last.game01987@gmail.com>
2022-11-24 17:28:25 +0800

    update template

--- components/DocumentTemplate.js
+++ components/DocumentTemplate.js
@@ -7,8 +7,11 @@ import {
   Image,
 } from "@react-pdf/renderer/lib/react-pdf.browser.cjs.js";
 import { logo } from "../assets";
+import { formatDate, formatTime } from "../utils/formatTime";
+import { countTotal } from "../../mobile/utils/countTotal";
 
 const DocumentTemplate = ({ data, week, realdate }) => {
+  const total = countTotal(data);
   return (
     <Document>
       <Page size="A4" style={styles.page}>
@@ -27,36 +30,60 @@ const DocumentTemplate = ({ data, week, realdate }) => {
           <View
             style={[
               styles.table,
-              { fontSize: 11, borderTop: 1, borderRight: 1, borderLeft: 1 },
+              { borderTop: 1, borderRight: 1, borderLeft: 1 },
             ]}>
             <View
               style={{
                 flexDirection: "row",
                 borderBottom: 1,
               }}>
-              <Text style={[styles.column, styles.column1]}>No.</Text>
-              <Text style={[styles.column, styles.column2]}>
+              <Text
+                style={[styles.column, styles.columnHeader, styles.column1]}>
+                No.
+              </Text>
+              <Text
+                style={[styles.column, styles.columnHeader, styles.column2]}>
                 Transaction Id
               </Text>
-              <Text style={[styles.column, styles.column3]}>Sender</Text>
-              <Text style={[styles.column, styles.column4]}>Amount(RM)</Text>
+              <Text
+                style={[styles.column, styles.columnHeader, styles.column3]}>
+                Sender
+              </Text>
+              <Text
+                style={[styles.column, styles.columnHeader, styles.column4]}>
+                Time
+              </Text>
+              <Text
+                style={[styles.column, styles.columnHeader, styles.column5]}>
+                Amount(RM)
+              </Text>
             </View>
             {data?.map((transaction, i) => (
-              <View key={i} style={{ flexDirection: "row", borderBottom: 1 }}>
+              <View key={i} style={styles.row}>
                 <Text style={[styles.column, styles.column1]}>{i + 1}.</Text>
                 <Text style={[styles.column, styles.column2]}>
                   {transaction.transaction_id}
                 </Text>
                 <Text style={[styles.column, styles.column3]}>
-                  {transaction.student_name}
+                  {transaction.student_name} ({transaction.sender})
                 </Text>
                 <Text style={[styles.column, styles.column4]}>
+                  {formatDate(transaction.created_on)} -{" "}
+                  {formatTime(transaction.created_at)}
+                </Text>
+                <Text style={[styles.column, styles.column5]}>
                   {transaction.amount}
                 </Text>
               </View>
             ))}
+            <View style={styles.row}>
+              <Text style={[styles.column, { flex: 20, textAlign: "right" }]}>
+                Total
+              </Text>
+              <Text style={[styles.column, styles.column5]}>{total}.00</Text>
+            </View>
           </View>
-          <Text style={{ fontSize: 11, marginTop: 24 }}>
+          <Text style={{ marginTop: 24 }}>
             This document printed on {realdate}
           </Text>
         </View>
@@ -72,28 +99,42 @@ const styles = StyleSheet.create({
   },
   table: {
     flexWrap: "wrap",
+    fontSize: 9,
+  },
+  row: {
+    flexDirection: "row",
+    borderBottom: 1,
   },
   column: {
     paddingHorizontal: 6,
-    paddingVertical: 3,
+    paddingVertical: 4,
     borderRight: 1,
-    fontSize: 10,
+    fontSize: 7,
+  },
+  columnHeader: {
+    fontWeight: 700,
   },
   column1: {
-    // width: 38,
     flex: 1,
     textAlign: "center",
   },
   column2: {
-    flex: 8,
+    flex: 7,
   },
   column3: {
     flex: 8,
   },
   column4: {
-    flex: 2,
+    flex: 3,
+  },
+  column5: {
+    width: 50,
     textAlign: "center",
     borderRight: 0,
+  },
+  image: {
+    width: "50%",
+    padding: 10,
   },
 });
 
