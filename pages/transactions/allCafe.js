caddb3f1c8a1e000a991ab897ccb79f84dba0ede
Haziq Musa <last.game01987@gmail.com>
2023-02-03 05:01:11 +0800

    refactor & update

--- pages/transactions/allCafe.js
+++ pages/transactions/allCafe.js
@@ -1,41 +1,111 @@
-import React from "react";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
 import { Layout, Button } from "../../components";
-
-// todo
-// buat first day of week jadi ahad & last day of week jdi sabtu
-// https://stackoverflow.com/questions/57799597/moment-js-set-week-start-on-monday
+import {
+  getOverallCafeTransactions,
+  getOverallCafeTransactionsFilter,
+} from "../../lib/getTransactions";
+import Link from "next/link";
 
 const AllCafe = () => {
+  const [cafe, setCafe] = useState({});
+  const [target, setTarget] = useState("all");
+
+  const onSelect = e => {
+    let target = e.target.value;
+
+    if (target === "all") {
+      getOverallCafeTransactions()
+        .then(data => {
+          setCafe(data);
+          setTarget("all");
+        })
+        .catch(err => err);
+    }
+
+    if (target === "week") {
+      const startOfWeek = moment(moment().startOf("week")).format("YYYY-MM-DD");
+      const endOfWeek = moment(moment().endOf("week")).format("YYYY-MM-DD");
+
+      getOverallCafeTransactionsFilter(startOfWeek, endOfWeek)
+        .then(data => {
+          setCafe(data);
+          setTarget("week");
+        })
+        .catch(err => err);
+    }
+
+    if (target === "month") {
+      const startOfWeek = moment(moment().startOf("month")).format(
+        "YYYY-MM-DD"
+      );
+      const endOfWeek = moment(moment().endOf("month")).format("YYYY-MM-DD");
+
+      getOverallCafeTransactionsFilter(startOfWeek, endOfWeek)
+        .then(data => {
+          setCafe(data);
+          setTarget("month");
+        })
+        .catch(err => err);
+    }
+
+    if (target === "today") {
+      const today = moment().format("YYYY-MM-DD");
+
+      getOverallCafeTransactionsFilter(today, today)
+        .then(data => {
+          setCafe(data);
+          setTarget("today");
+        })
+        .catch(err => err);
+    }
+  };
+
+  useEffect(() => {
+    getOverallCafeTransactions()
+      .then(data => {
+        setCafe(data);
+      })
+      .catch(err => err);
+  }, []);
+
   return (
     <Layout>
       <div className="w-2/3 items-center my-6">
         <h1 className="mb-[30px] font-bold text-3xl">
           Transactions List (All Cafe)
         </h1>
-        <div className="mb-3">
-          <select
-            // value={selects.value}
-            // onChange={onSelect}
-            className="py-2 px-3 mr-2 border-[1px] rounded-md bg-[#FFFFFF] border-gray-300">
-            <option value="today">Today</option>
-            <option value="month">Month</option>
-          </select>
-          <select
-            // value={selects.value}
-            // onChange={onSelect}
-            className="py-2 px-3 mr-2 border-[1px] rounded-md bg-[#FFFFFF] border-gray-300">
-            <option>Choose week</option>
-            <option value="week1">Week 1</option>
-            <option value="week2">Week 2</option>
-            <option value="week3">Week 3</option>
-            <option value="week4">Week 4</option>
-          </select>
-          {/* <Link
-            href={`/transactions/pdf/${username}?from=${date[0]}&to=${date[1]}`}>
-            <a target="_blank" rel="noopener noreferrer"> */}
-          <Button>Print</Button>
-          {/* </a>
-          </Link> */}
+        <div className="flex justify-between gap-3 items-center mb-3">
+          <div>
+            <select
+              onChange={onSelect}
+              className="py-2 px-3 mr-2 border-[1px] rounded-md bg-[#FFFFFF] border-gray-300">
+              <option value="all">All</option>
+              <option value="today">Today</option>
+              <option value="week">This Week</option>
+              <option value="month">Month</option>
+            </select>
+            <Link href={`/transactions/pdf/cafe/${target}`}>
+              <a target="_blank" rel="noopener noreferrer">
+                <Button>Print</Button>
+              </a>
+            </Link>
+          </div>
+          <div className="flex gap-2">
+            <span className="py-2 px-3 border-[1px] rounded-md bg-[#FFFFFF] border-gray-300">
+              <label htmlFor="from" className="mr-2">
+                From
+              </label>
+              <input type="date" id="from" />
+            </span>
+            <span className="py-2 px-3 border-[1px] rounded-md bg-[#FFFFFF] border-gray-300">
+              <label htmlFor="to" className="mr-2">
+                To
+              </label>
+              <input type="date" id="to" />
+            </span>
+            <Button>Find</Button>
+          </div>
         </div>
         <div className="p-8 border-[1px] rounded-md bg-[#FFFFFF] border-gray-300">
           <table className="centertable">
@@ -50,22 +120,28 @@ const AllCafe = () => {
               </tr>
             </thead>
             <tbody>
-              {/* {transactions?.map((data, i) => { */}
-              {/* return ( */}
-              <tr className="text-gray-500">
-                <td className="pb-6 pr-4 text-center">1</td>
-                <td className="pb-6">Cafe 1</td>
-                <td className="pb-6 text-center">2</td>
-                <td className="pb-6 text-center">4</td>
-              </tr>
-              {/* ); */}
-              {/* })} */}
+              {cafe?.transactions?.map((data, i) => {
+                return (
+                  <tr key={i} className="text-gray-500">
+                    <td className="pb-6 pr-4 text-center">{i + 1}</td>
+                    <td className="pb-6">{data.cafe_name}</td>
+                    <td className="pb-6 text-center">
+                      {data.total_transaction}
+                    </td>
+                    <td className="pb-6 text-center">{data.total_amount}</td>
+                  </tr>
+                );
+              })}
               <tr className="text-gray-500">
                 <td colSpan={2} className="py-6 text-right">
                   Total
                 </td>
-                <td className="py-6 text-center">2</td>
-                <td className="py-6 text-center">4</td>
+                <td className="py-6 text-center">
+                  {cafe?.overall?.sum_transaction}
+                </td>
+                <td className="py-6 text-center">
+                  {cafe?.overall?.sum_amount}
+                </td>
               </tr>
             </tbody>
           </table>
